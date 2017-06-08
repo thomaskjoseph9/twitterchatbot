1bc52586e0f09cc6bf9f249ff5abedc50c6c82f6
thomas <thomas@massively.ai>
2017-06-08 13:12:12 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {  
+  consumer_key: 'IS1WVkhqv1AGkeVNv37EpaPgo',  
+  consumer_secret: 'KO3gFSGayHseN08UNoSGOWrN6agLRueIR9G3g59PTtY0lfmA4d',
+  access_token: '872118160773795840-itNMJgZam7okDMkaygKDlDyxUu5ONZY',  
+  access_token_secret: 'Gyy39N7GGVO5WavrAUcqrrGNZigdKk8e5n9yVqOOXCnnP'
+}
